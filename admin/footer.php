47831695aa8401c13997ac4a686f52ede11e4569
HoangAnh <90691318+HoanggAnh@users.noreply.github.com>
2022-06-14 18:31:08 +0700

    update

--- admin/footer.php
+++ admin/footer.php
@@ -2,4 +2,4 @@
         <div class="pull-right hidden-xs">
           <b style="color: white">Copyright &copy; 2021</b>
         </div>
-        <strong style="color: #f3906c">Đề tài 3: Xây dựng Website giới thiệu và đặt sách trực tuyến</strong>
+        <strong style="color: #f3906c">Diamonds Book Store</strong>
